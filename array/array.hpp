12f0fcc66f916bae11d38120829645fd0754b085
6disquiet <kpolcyn.n@gmail.com>
2023-03-14 20:53:40 +0100

    Fixed copy constructor, created at method

--- array/array.hpp
+++ array/array.hpp
@@ -11,11 +11,13 @@ class Array{
     Array(T);
     Array(T, T);
     Array(const Array&);
-
     void show();
+    T operator[](int) const;
+    T at(int);
 
 };
 
+//default constructor
 template<typename T, int N>
 Array<T,N>::Array(){
     T default_value{};
@@ -24,14 +26,14 @@ Array<T,N>::Array(){
     }
 }
 
-
+//constructor with parameter
 template<typename T, int N>
 Array<T,N>::Array(T x){
     for(int i = 0 ; i < N; i++){
         data[i] = x;
     }
 }
-
+//constructor with two parameters
 template<typename T, int N>
 Array<T, N>::Array(T x, T y){
     for(int i = 0; i <N; i+=2){
@@ -39,18 +41,27 @@ Array<T, N>::Array(T x, T y){
         data[i+1] = y;
     }
 }
-
+//copy constructor
 template<typename T, int N>
 Array<T, N>::Array(const Array& x){
     for(int i = 0; i < N; i++){
         data[i] = x[i];
     }
 }
-
-
+//overload[] operator
+template<typename T, int N>
+T Array<T, N>::operator[](int x) const{
+    return data[x];
+}
+//show method
 template<typename T, int N>
 void Array<T,N>::show(){
         for(int i = 0; i < N; i++){
             std::cout<<data[i];
         } 
+}
+
+template<typename T, int N>
+T Array<T, N>::at(int i){
+    return data[i];
 }
--- main.cpp
+++ main.cpp
@@ -6,10 +6,12 @@ int main(){
     a.show();
 
     Array<int, 10> b(1, 2);
+    std::cout << std::endl << b[3] << std::endl;
     b.show(); 
 
     Array<int, 10> de = a;
     de.show();
 
+    std::cout<< std::endl << de.at(3) << std::endl;
 
 }
