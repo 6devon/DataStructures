080a5c09e83eaf98815649e460296da0c14e5049
6disquiet <kpolcyn.n@gmail.com>
2023-06-05 22:03:25 +0200

    Added copy constructor and assigment operator

--- vector/vector.hpp
+++ vector/vector.hpp
@@ -12,6 +12,8 @@ private:
 public:
     Vector();
     Vector(int initialCapacity);
+    Vector(const Vector& other);
+    Vector& operator=(const Vector& other);
     ~Vector();
     // T at(int);
     // T& operator[](int);
@@ -24,8 +26,35 @@ template<typename T>
 Vector<T>::Vector(): elements(nullptr), size(0), capacity(0){ //null pointer value is used to indicate that a pointer does not point ro aany valid memory adress
 }
 template<typename T>
-Vector<T>::Vector(int initialCapacity): elements(nullptr), size(0), capacity(initialCapacity){ //null pointer value is used to indicate that a pointer does not point ro aany valid memory adress
+Vector<T>::Vector(int initialCapacity): elements(nullptr), size(0), capacity(initialCapacity){ //null pointer value is used to indicate that a pointer does not point or any valid memory adress
     elements = new T[capacity];
+}
+
+//copy constructor
+//copy constructor creates new Vector object by making a deep copy of existing Vector object
+//deep copy -> allocates new memory for object
+//shallow copy -> shares the same memory space
+template<typename T>
+Vector<T>::Vector(const Vector& other): elements(nullptr), size(other.size), capacity(other.capacity){
+    elements = new T[capacity];
+    for(int i = 0; i < size; ++i){
+        elements[i] = other.elements[i];
+    }
+}
+
+//assigment operator
+template<typename T>
+Vector<T>& Vector<T>::operator=(const Vector& other){
+    if(this != &other){
+        delete[] elements;
+        size = other.size();
+        capacity = other.capacity;
+        elements = new T[capacity];
+        for(int i = 0; i < size; ++i){
+            elements[i] = other.elements[i];
+        }
+    }
+    return *this;
 }
 
 //destructor implementation
